2d2825b8cc2f5780d7e079ce722f3ccfed13c575
José Ismar <ismar.jr@icloud.com>
2024-03-11 18:09:54 -0300

    js13 - Object revision
    revisando objetos em js

---
+++ js13 - Object revision/script.js
@@ -0,0 +1,15 @@
+// Object
+
+const person = {
+    name: 'Ismar',
+    age: 30,
+    weight: 88.6,
+    isAdmin: true
+};
+//Para mostrar um objeto basta colocar o nome dele 
+console.log(person);
+
+//Para mostrar um atributo desse objeto, precisa-se colocar o nome do objeto e o atributo, Ex: person.name;
+console.log(person.name);
+
+console.log(`${person.name} tem ${person.age} anos`);
