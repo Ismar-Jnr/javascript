fd8890afdaf98025a4cade4162212e99c35d9fa6
José Ismar <ismar.jr@icloud.com>
2024-03-11 14:45:34 -0300

    js03 - numbers
    entendendo numeros em js

---
+++ js03 - numbers/script.js
@@ -0,0 +1,17 @@
+/**
+ * Number
+    *números
+
+    33   // inteiros
+
+    12.5 // reais
+
+    NaN  // Not a Number
+
+    Infinity  // infinito
+ */
+
+    console.log(33); // numero inteiro
+    console.log(12.5); // numero real
+    console.log(10 / "sa"); //NaN
+    console.log(10 === Infinity); // Infinity é um valor de referencia. Obs: 1º "I" deve ser maiúsculo, do contrario é um erro de referencia.
