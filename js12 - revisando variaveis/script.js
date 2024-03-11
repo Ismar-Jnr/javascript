3fbe70476ae056c91dbf10e2379b8159d73cefdc
José Ismar <ismar.jr@icloud.com>
2024-03-11 18:00:13 -0300

    js12 - revisando variaveis
    revisando variaveis em js

---
+++ js12 - revisando variaveis/script.js
@@ -0,0 +1,23 @@
+// variaveis e tipos de dados
+
+    //declaration
+    var name;
+
+    //assignment values
+    name = "Ismar";
+
+    //type of data 
+    console.log(typeof name);
+
+
+    let age = 20, isHuman = true;
+
+    //Multiple Arguments in the Function
+    console.log(name, age, isHuman);
+
+    //concatenando valores 
+    console.log('O ' + name + ' tem ' + age + ' anos');
+    
+
+    //interpolando valores com template literals or template strings
+    console.log(`o ${name} tem ${age} anos`);
