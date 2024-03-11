2f9adbd04ee19260946b3c80adfdb540fad5660e
José Ismar <ismar.jr@icloud.com>
2024-03-11 15:11:20 -0300

    js07 - Array
    entendendo arrays/vetores em js

---
+++ js07 - Array/script.js
@@ -0,0 +1,19 @@
+/**
+ * Array (Vetores)
+        Uma lista
+        Agrupamento de dado
+
+        ["Ismar", 25]        
+ */
+
+console.log([
+    "Ismar",
+     25
+]);
+
+console.log([
+    "Leite",
+    "Ovos",
+    2,
+    3
+]);
