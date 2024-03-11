ea4863e145beb416fc3127f15b4ab692bf331df2
José Ismar <ismar.jr@icloud.com>
2024-03-11 14:54:55 -0300

    js05 - null e undefined
    entendendo null e undefined

---
+++ js05- null e undefined/script.js
@@ -0,0 +1,14 @@
+/**
+ * Undefined  //  indefinido
+ * 
+    
+
+ *null
+    * nulo
+    * objeto que nao possui nada dentro
+    * diferente de indefinido
+ */
+
+ console.log(undefined); //
+ console.log(null); //Considerado como um objeto que nao tem nada dentro dele
+ console.log(null === undefined); // false
