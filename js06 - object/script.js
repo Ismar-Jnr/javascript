f02c33b7a59a6d1a776a139b3a5bdd9c0886bd5a
José Ismar <ismar.jr@icloud.com>
2024-03-11 15:05:23 -0300

    js06 - Object
    entendendo object em js

---
+++ js06 - object/script.js
@@ -0,0 +1,18 @@
+/**
+ * Object
+        * Objeto
+        * Propriedades / Atributos
+        * Funcionalidades / Métodos
+        
+    { propriedade: "valor"}
+
+
+ */
+
+    console.log({
+        name: "Ismar", // Criei um objeto, com uma propriedade "name", que possui o valor "Ismar";
+        idade: 25, // Adicionei a este objeto uma propriedade "idade", com valor "25";
+        andar: function(){
+            console.log('andar');
+        } // Agora, adicionei a este objeto uma funcionalidade/método "andar", que possui uma função/function "andar;"
+    }) ;
