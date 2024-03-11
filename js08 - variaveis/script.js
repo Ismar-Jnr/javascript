51ec030e5c31e4a8a2993ea77ac7459b6f9cb55d
José Ismar <ismar.jr@icloud.com>
2024-03-11 16:23:17 -0300

    js08 - variaveis
    entendendo variaveis em js

---
+++ js08 - variaveis/script.js
@@ -0,0 +1,26 @@
+/**
+ *  # Variáveis
+        ou indentificadores
+
+    * 3 palavras reservadas para criar variaveis
+        * var
+        * let
+        * const  //variaveis const não podem mudar seu valor durante a aplicação
+        * 
+        
+    O JS é uma linguagem fracamente tipada e dinâmica, logo as variaveis não precisam ter um tipo previamente definido
+
+    Pode-se mudar o conteúdo da variavel (Exceto variasveis const (constante))
+
+    as variaveis no js, recebem um tipo de acordo com o valor que recebe.
+    sendo assim, a variavel pode mudar de tipo de acordo com a mudança de valores.
+ */
+let clima = "Quente"; //String
+console.log(clima);
+
+clima = "Frio"; // String
+console.log(clima);
+
+clima = true; // boolean
+console.log(clima);
+
