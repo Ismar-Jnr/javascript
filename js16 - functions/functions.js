d5ec8bbac5de63526feb05f8ff30cf967fe77f20
José Ismar <ismar.jr@icloud.com>
2024-03-16 18:54:15 -0300

    js16 - functions arguments
    Passagem de argumentos por parâmetros

---
+++ js16 - functions/functions.js
@@ -0,0 +1,13 @@
+//Tambem é possivel declarar funções dentro de variaveis
+//são chamadas de function anonymous ou function expression
+const sum = function(number1, number2/*Parametros da função*/) {
+    console.log(number1 + number2);
+};
+/*******************/
+
+//rodando a função
+//running function
+sum(2, 3); 
+
+//Na linha acima, ao chamar a função e inserir os numeros, estamos passando argumentos para a função. Passando argumentos por parametros.
+/*******************/
