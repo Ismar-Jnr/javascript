d19fd2e6ed260703d3897b8ff6a2503b518da647
José Ismar <ismar.jr@icloud.com>
2024-03-16 20:35:19 -0300

    js16 - update functions
    Passagem de argumentos para parametros

---
+++ js16 - functions/funcions-02.js
@@ -0,0 +1,20 @@
+//Esse arquivo contem outra explicação sobre funções
+
+//Imagine que a função é um liquidificador
+
+function fazerSuco (fruta01/*Parametro 1 */, fruta02/*Parametro 2 */) {
+    return fruta01 + fruta02;
+};
+
+const copo = fazerSuco('banana', 'maça');
+//banana foi passado como argumento para o parametro 1
+//  maça foi passado como argumento para o parametro 2
+
+console.log(copo);
+
+//a variavel copo recebeu a função "fazerSuco" com os argumentos e teve o retorno da função
+// return fruta01 + fruta02;
+
+
+
+
