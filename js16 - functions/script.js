23efc3cdfeecd6528dce8c595f1dbad4d13165da
José Ismar <ismar.jr@icloud.com>
2024-03-16 18:41:41 -0300

    js16 - functions
    Introdução a funções em js

---
+++ js16 - functions/script.js
@@ -0,0 +1,23 @@
+// function tambem é um tipo de dado
+
+// Aprendendo a declarar e executar função 
+
+// Criar um aplicativo de frases motivacionais
+// De nomes com significados
+
+// declaration - declaração da função ou tambem funcion statement
+function createPhrases() {
+    console.log('Estudar é muito bom');
+    console.log('Paciência e Persistência');
+    console.log('Revisão é a mãe do aprendizado');    
+};
+
+// rodar, chamar, invocar ou
+// executar a função 
+//execute, run, call, invoke
+createPhrases();
+createPhrases(); // Cada chamada da função replica ela no console
+createPhrases();
+
+console.log('Fim do programa');
+
