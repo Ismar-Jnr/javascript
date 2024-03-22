cda2e04dee726d52d71fad1bb2a7266ad3ac42c1
José Ismar <ismar.jr@icloud.com>
2024-03-22 10:03:26 -0300

    js20 - callback function
    complementando callback function

--- js20 - callback function/scripts.js
+++ js20 - callback function/scripts.js
@@ -3,7 +3,8 @@
 function sayMyName(name){
 
     console.log('antes de executar a função callback')
-    name()
+    
+    name() //esse parametro recebe a callback, que vem da função abaixo, e executa a função, dentro da função primaria.
     
 
     console.log('depois de executar a função callback')
@@ -11,8 +12,9 @@ function sayMyName(name){
 
 //pode-se passar todos os tipo de dados para uma função, inclusive outra função
 //no caso abaixo, estamos passando uma função arrow para outra função
+
 sayMyName(
     () => {
         console.log('estou em uma callback')
-    }
+    } // essa função foi passada como parametro para função anterior - no caso, para o parametro "name".
 )
