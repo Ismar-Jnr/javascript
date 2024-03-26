bf94cae5fc530e97eb97312a9e7373ae6de6741c
José Ismar <ismar.jr@icloud.com>
2024-03-26 17:39:22 -0300

    js28 - create array with constructor
    aprendendo a criar um array com construtor

---
+++ js28 - create array with constructor/scripts.js
@@ -0,0 +1,18 @@
+// Manipulando Arrays
+
+// Criar array com construtor
+
+
+//array comum
+let myArray = ['a', 'b', 'c'];
+console.log(myArray);
+
+
+//array com construtor
+let myNewArray = new Array('y', 'x', 'z');
+console.log(myNewArray);
+
+
+//podemos tambem crair um array vazio
+let vazio = new Array(10); //cria um array com 10 posições vazias
+console.log(vazio); 
