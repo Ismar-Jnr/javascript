e833017064dd514155d638bcc6b6f35c42f9239d
José Ismar <ismar.jr@icloud.com>
2024-03-26 16:49:06 -0300

    js25 - manipulation(uppercase e  lowercase)
    Usando toUpperCase e toLowerCase para manipular dados strings

---
+++ js25 - manipulation (uppercase-lowercase)/scripts.js
@@ -0,0 +1,24 @@
+// Manipulando Strings e Números
+
+//Transforme letras minpsculas em maiúsculas e vice-versa
+
+
+//Uppercase
+let word = "Programar é muito bacana!";
+console.log(word.toUpperCase());
+
+
+//Lowercase
+let word2 = "PROGRAMAR É MUITO BOM!"; 
+console.log(word2.toLowerCase());
+
+
+//Tambem é possivel atrelar 2 metodos a um unico objeto
+//Nesse exemplo, não faz nenhum sentido, mesmo assim, veja o exemplo abaixo: 
+
+let palavra = "Exemplo sem sentindo para fins didaticos";
+console.log(palavra.toLowerCase(). toUpperCase());
+
+//o primeiro metodo deixou tudo minusculo e o segundo tudo maiusculo.
+
+
