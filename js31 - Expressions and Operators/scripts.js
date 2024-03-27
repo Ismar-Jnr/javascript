cce43feb5a9abb9829902aabedbda656131e4163
José Ismar <ismar.jr@icloud.com>
2024-03-27 17:51:08 -0300

    js31 - Expressions and Operators
    Expressões e operadores em js

---
+++ js31 - Expressions and Operators/scripts.js
@@ -0,0 +1,26 @@
+/* Expressões e Operadores 
+
+    - Expressions
+    - Operators
+            Binary 
+            Unary
+            Ternary
+*/
+
+let number = 1;
+
+(function(){  //função alto executavel
+    console.log('Alo')
+})()
+
+//Binary
+console.log(number + 1)
+
+
+//Unary
+console.log(++number)
+console.log(typeof number)
+
+
+//Ternary
+console.log(false ? 'alo' : 'nada')
