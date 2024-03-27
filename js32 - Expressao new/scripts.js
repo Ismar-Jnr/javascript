d6694338c2762f9909fe635107da4de8f27c980c
José Ismar <ismar.jr@icloud.com>
2024-03-27 20:55:46 -0300

    js32 - expression new
    como ja vimos antes, new é um construtor, neste arquivo vimos exemplo da expressão new criando alguns objetos

---
+++ js32 - Expressao new/scripts.js
@@ -0,0 +1,17 @@
+/*
+
+    new
+
+    *left-hand-side expression
+
+    *new serve para criar um novo objeto
+ */
+
+    let name = new String('Ismar');
+    name.surName = "Brito"
+    let age = new Number(25)
+    console.log(name.surName, age)
+
+    let data = new Date('2020-12-01')
+    console.log(data)
+/*---------------------------------------------------------------------------*/
