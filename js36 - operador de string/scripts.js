8c085eac310b90a9b8d2578c8018f42d1858c199
José Ismar <ismar.jr@icloud.com>
2024-03-27 21:51:13 -0300

    js36 - string operator
    praticando

---
+++ js36 - operador de string/scripts.js
@@ -0,0 +1,23 @@
+// Operador de String (String Operator)
+
+// comparision (comparação)
+    console.log('a' == 'b'); //false
+    console.log('a' == 'a'); //true
+
+// concatenation (concatenção)
+// Retorna união de duas strings
+
+console.log('a' + 'a'); // 'aa'
+
+
+
+let a = 'alpha';
+console.log(a + 'bet'); // 'alphabet'
+
+
+
+let b = 'verdade';
+b += 'iro';
+console.log(b); // 'verdadeiro'
+
+
